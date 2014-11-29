81bcc3260e5166fc112b83e877ec9197765c6794
Jonas Kunze <kunze.jonas@gmail.com>
2014-11-30 00:12:57 +0100

    Storing EvenInfo like sob in a map in case of a mess up

---
+++ src/EventInfo.h
@@ -0,0 +1,17 @@
+/*
+ * EventInfo.h
+ *
+ *  Created on: Nov 29, 2014
+ *      Author: root
+ */
+
+#ifndef STRUCTS_EVENTINFO_H_
+#define STRUCTS_EVENTINFO_H_
+
+struct EventInfo {
+	uint32_t burstID;
+	uint runNumber;
+	uint sob;
+};
+
+#endif /* STRUCTS_EVENTINFO_H_ */
